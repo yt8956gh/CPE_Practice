522ea3bcd2a42cc520d9f71c33c9c75bae7657fe
nian <yt8956789@gmail.com>
2020-05-08 14:25:23 +0800

    upload .c file on my laptop

---
+++ 100/100.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int floor=0,ceil=0,max=0,count=0; 
+
+    while(scanf("%d %d", &floor, &ceil)!=EOF)
+    {
+        max=0;
+        printf("%d %d ", floor, ceil);
+        for(int i=floor;i<=ceil;i++)
+        {
+            count=0;
+            while()
+        }
+    }
+
+    return 0;
+}
